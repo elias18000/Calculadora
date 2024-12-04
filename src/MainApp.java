b183070becc81ac7b18f696c90f97ed0eac7a4d2
Elias Wassit <ewascal004@g.educaand.es>
2024-12-04 09:59:59 +0100

    Creacion multiplicacion y division

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,14 +7,22 @@ public class MainApp {
         System.out.println("Dame el segundo numero a operar");
         int numero2 = Integer.parseInt(scan.nextLine());
         
-        System.out.println("el resultado de la suma es" + suma(numero1, numero2));
-        System.out.println("el resultado de la resta es" + resta(numero1, numero2));    
+        System.out.println("el resultado de la suma es: " + suma(numero1, numero2));
+        System.out.println("el resultado de la resta es: " + resta(numero1, numero2));  
+        System.out.println("el resultado de la multiplicacion es: " + multiplicacion(numero1, numero2));  
+        System.out.println("el resultado de la operacio es: " + division(numero1, numero2));
     }
     public static int suma(int numero1, int numero2){
         return numero1 + numero2;
     }
     public static int resta(int numero1, int numero2){
         return numero1 - numero2;
+    }
+    public static int multiplicacion(int numero1, int numero2){
+        return numero1 * numero2;
+    }
+    public static int division(int numero1, int numero2){
+        return numero1 / numero2;
     }
 
 }
